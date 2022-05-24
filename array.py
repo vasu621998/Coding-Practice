98265c7750441ff23c8be6c0a938b905e2f3a36a
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-05-23 22:02:02 -0400

    Update array.py
    Duplicate Zeros

--- array.py
+++ array.py
@@ -24,3 +24,16 @@ class Solution:
             if( digit % 2 == 0):
                 count +=1
         return count   
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i=0
+        
+        while(i<len(arr)):
+            if(arr[i] != 0):
+                i+=1
+            else:
+                arr.insert(i+1, 0)
+                i+=2
+                arr.pop()
