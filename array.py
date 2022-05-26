cf34d03a10f0d53a55b89ae935329caca48f4a79
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-05-25 21:31:52 -0400

    Update array.py
    Replace Elements with Greatest Element on Right Side

--- array.py
+++ array.py
@@ -79,3 +79,10 @@ class Solution:
                 if num > prevNum: return False
         
         return climbed and peakReached
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1):
+            arr[i] = max(arr[i+1:len(arr)])
+        
+        arr.pop()
+        arr.append(-1)
+        return arr
