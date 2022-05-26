aa822167eb9d6ac00738b15a4c8caea8e64b41a4
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-05-26 13:23:46 -0400

    Update array.py
    Sort Array By Parity

--- array.py
+++ array.py
@@ -94,3 +94,16 @@ class Solution:
             if i == 0:
                 nums.remove(i)
                 nums.append(i)
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        i = 0
+        j = len(nums)-1
+        while i<j:
+            if nums[i]%2 != 0 and nums[j]%2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+                j-=1
+            elif nums[i]%2 == 0:
+                i+=1
+            elif nums[j]%2 != 0:
+                j-=1
+        return nums
