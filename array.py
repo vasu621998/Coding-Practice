e3657cf0b5ed21b89a0dede411f40ddb81c78872
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-05-27 18:23:16 -0400

    Update array.py
    Find All Numbers Disappeared in an Array

--- array.py
+++ array.py
@@ -129,3 +129,16 @@ class Solution:
             return max(nums)
         
         return sorted(list(nums))[-3]  
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        dic = {}
+        n = len(nums)
+        
+        for i in range(n):
+            dic[nums[i]] = nums[i]
+        lst = []  
+        
+        for i in range(1, n+1):
+            if i not in dic:
+                lst.append(i)
+        return lst
+  
