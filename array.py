0fda7aed5b602026f4a54cd28b7036bc326b6df2
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-05-27 23:45:18 -0400

    Update array.py
    Find Pivot Index

--- array.py
+++ array.py
@@ -142,3 +142,11 @@ class Solution:
                 lst.append(i)
         return lst
  ### Starting Arrays and Strings chapter in Leetcode 
+    def pivotIndex(self, nums: List[int]) -> int:
+        s1,s2=0,sum(nums)
+        for i in range(len(nums)):
+            s2-=nums[i]
+            if s1==s2:
+                return i
+            s1+=nums[i]
+        return -1
