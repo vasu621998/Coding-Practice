12dafbf9a900691154d22e4c0b60aaabc68f35d8
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-05-31 21:14:14 -0400

    Reverse String
    Write a function that reverses a string. The input string is given as an array of characters s.
    
    You must do this by modifying the input array in-place with O(1) extra memory.

--- string.py
+++ string.py
@@ -35,3 +35,20 @@ class Solution:
             
             res += strs[0][i]
         return res
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        #return s.reverse() ##Reverse method
+        #s[:] = s[-1:-1] ## Slicing method
+        ##Using two pointers:
+        
+        #first pointer
+        ptr1 = 0
+        #Second pointer 
+        ptr2 = len(s) - 1
+        
+        while ptr1 < ptr2:
+            s[ptr1], s[ptr2] = s[ptr2], s[ptr1]
+            ptr1 += 1
+            ptr2 -= 1
