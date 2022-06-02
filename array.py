bbc85042def5d72f9ed9fcb2d6230867e5496570
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-01 22:44:24 -0400

    Rotate Array
    Given an array, rotate the array to the right by k steps, where k is non-negative.

--- array.py
+++ array.py
@@ -267,3 +267,25 @@ class Solution:
                 ptr1 +=1
             else:
                 return [ptr1 + 1, ptr2 + 1]
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        lptr, rptr = 0 , len(nums) - 1
+        while lptr < rptr:
+            nums[lptr], nums[rptr] = nums[rptr], nums[lptr]
+            lptr += 1
+            rptr -= 1
+        
+        lptr, rptr = 0 , k - 1
+        while lptr < rptr:
+            nums[lptr], nums[rptr] = nums[rptr], nums[lptr]
+            lptr += 1
+            rptr -= 1
+            
+        lptr, rptr = k , len(nums) - 1
+        while lptr < rptr:
+            nums[lptr], nums[rptr] = nums[rptr], nums[lptr]
+            lptr += 1
+            rptr -= 1
