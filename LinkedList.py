ab750d8ec1c8a0e7b6e19ac41b2828dc439403ed
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-04 10:40:52 -0400

    Linked List Cycle II
    Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
    
    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
    
    Do not modify the linked list.

--- LinkedList.py
+++ LinkedList.py
@@ -87,3 +87,17 @@ class Solution:
 # obj.addAtTail(val)
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        pointer = head
+        while pointer != fast:
+            pointer = pointer.next
+            fast = fast.next
+        return pointer
