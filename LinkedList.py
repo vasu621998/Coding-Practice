48d3db164feb6861b82d7de739acc57ffb7cbe59
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-05 16:48:47 -0400

    Palindrome Linked List
    Given the head of a singly linked list, return true if it is a palindrome.

--- LinkedList.py
+++ LinkedList.py
@@ -171,3 +171,24 @@ class Solution:
         oddList.next = evenhead
         
         return head
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow,fast = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        left,right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
