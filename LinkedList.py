73caeaabf299ba2068df7fe0edfb309bd251a076
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-06 20:29:50 -0400

    Rotate List
    Given the head of a linked list, rotate the list to the right by k places.

--- LinkedList.py
+++ LinkedList.py
@@ -452,3 +452,25 @@ class DoublyLinkedList:
             copy.random = myHashMap[curr.random]
             curr = curr.next
         return myHashMap[head]
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head  
+        
+        length, tail = 1, head
+        while tail.next:
+            tail = tail.next
+            length += 1
+        
+        k = k % length
+        if k == 0 :
+            return head
+        
+        curr = head
+        
+        for i in range(length - k - 1):
+            curr = curr.next
+        newHead = curr.next
+        curr.next = None
+        tail.next = head
+        
+        return newHead
