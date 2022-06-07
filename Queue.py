6d961bf108a501df2cbabd80ba7d15dd3a25b3c7
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-07 18:21:32 -0400

    Number of Islands
    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
    
    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.

--- Queue.py
+++ Queue.py
@@ -51,3 +51,36 @@ class MyCircularQueue:
 # param_4 = obj.Rear()
 # param_5 = obj.isEmpty()
 # param_6 = obj.isFull()
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        island = 0
+        visit = set()
+        rows, cols = len(grid), len(grid[0])
+        
+        def bfs(r,c):
+            q = collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r  = row + dr
+                    c = col + dc
+                    if(r in range(rows) and
+                      c in range(cols) and
+                      grid[r][c] == "1" and
+                      (r,c) not in visit):
+                        visit.add((r,c))
+                        q.append((r,c))
+                    
+            
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    island += 1
+        return island
