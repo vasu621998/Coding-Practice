2f134b346c9b346a03fd18566fec4f590020549a
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-08 18:05:21 -0400

    Perfect Squares
    Given an integer n, return the least number of perfect square numbers that sum to n.
    
    A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.

--- Queue.py
+++ Queue.py
@@ -111,3 +111,15 @@ class MyCircularQueue:
                     visit.add(child)
                     q.append([child, turns + 1])
         return -1
+    def numSquares(self, n: int) -> int:
+        arr = [n] * (n + 1)
+        arr[0] = 0
+        
+        for target in range(1, n + 1):
+            for s in range(1, target + 1):
+                square = s * s
+                if target - square < 0:
+                    break
+                arr[target] = min(arr[target] , 1 + arr[target - square])
+                
+        return arr[n]
