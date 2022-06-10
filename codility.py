98d991d22091f7e20e1fd54cd03080e788b58f15
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-10 00:30:01 -0400

    Count Divisibles
    Count Divisibles

--- codility.py
+++ codility.py
@@ -104,17 +104,11 @@ def solution(A):
             return A[i] + 1
     return A[len(A) - 1] + 1
 
-# Binray Gap
-def Solution(N):
-  n = bin(N) [2:]
-  b = 0
-  count = 0
-  
-  for i in N:
-    if int(i) == 0:
-      b += 1
-     elif int(i) == 1:
-      count = max(b, count)
-      b = 0
-  return count                     
+# Count Divisibles
+def Solution(A,B,K):
+  c = int(B/K) - int(A/K)
+  if(A%K == 0):
+        c = c+1
+  return c
+                     
                    
