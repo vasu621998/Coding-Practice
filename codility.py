340219405155c7c53b613997d4ca5a5288f300ee
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-09 22:21:58 -0400

    Max counters
    Max counters

--- codility.py
+++ codility.py
@@ -71,3 +71,20 @@ def solution(X, A):
         if len(leaves) == X:
             return second
     return -1
+
+# Max counters
+def solution(N, A):
+    R = [0] * N 
+    m = 0
+    b = 0
+    for i in range(0, len(A)):
+      if A[i] <= N:
+        R[A[i] - 1] = max(b, R[A[i] - 1]) + 1
+        m = max(m, R[A[i] - 1)
+      else:
+         b = m
+    for i in range(0, len(R)):
+        if (R[i] < b):
+           R[i] = b   
+         
+        
