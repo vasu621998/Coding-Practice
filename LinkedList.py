90a27bb40ab645533561195de173e2da1e0a87aa
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-10 19:06:23 -0400

    Sort List
    Given the head of a linked list, return the list after sorting it in ascending order.

--- LinkedList.py
+++ LinkedList.py
@@ -474,3 +474,45 @@ class DoublyLinkedList:
         tail.next = head
         
         return newHead
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        left = head
+        right = self.getMid(head)
+        
+        tmp = right.next
+        right.next = None
+        right = tmp
+        
+        left = self.sortList(left)
+        right = self.sortList(right)
+        
+        return self.merge(left,right)
+        
+    def getMid(self, head):
+        
+        ptr1 = head
+        ptr2 = head.next
+        
+        while ptr2 and ptr2.next:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next.next
+            
+        return ptr1
+    
+    def merge(self, list1, list2):
+        tail = dummy = ListNode()
+        while list1 and list2 :
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        return dummy.next
