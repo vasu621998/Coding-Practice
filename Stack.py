f08181deddf74c8fe29bc5d3ed88e9e29c4a5fe9
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-11 20:12:02 -0400

    Daily Temperatures
    Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.

--- Stack.py
+++ Stack.py
@@ -43,3 +43,14 @@ class Solution:
                 stack.append(c)
         
         return True if not stack else False
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        
+        for i,t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackI = stack.pop()
+                res[stackI] = ( i - stackI)
+            stack.append([t, i])
+            
+        return res
