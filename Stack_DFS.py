ffcb80f1b4ce987eda3cdf627a42136c2d891bbe
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-12 23:14:14 -0400

    Target Sum
    You are given an integer array nums and an integer target.
    
    You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
    
    For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
    Return the number of different expressions that you can build, which evaluates to target.

--- Stack_DFS.py
+++ Stack_DFS.py
@@ -92,3 +92,16 @@ class Node:
             return copy
         
         return dfs(node) if node else None
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = {} #(index,total)
+        
+        def backtrack(index, total):
+            if index == len(nums):
+                return 1 if total==target else 0
+            if (index,total) in dp:
+                return dp[(index,total)]
+            dp[(index,total)] = (backtrack(index+1, total + nums[index]) +
+                                 backtrack(index+1, total - nums[index]) )
+            
+            return dp[(index, total)]
+        return backtrack(0,0)
