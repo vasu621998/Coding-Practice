40f89f8bab5397b88124e3565a28819a10c0656e
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-14 09:21:58 -0400

    01 Matrix
    Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
    
    The distance between two adjacent cells is 1.

--- Queue_BFS.py
+++ Queue_BFS.py
@@ -123,3 +123,26 @@ class MyCircularQueue:
                 arr[target] = min(arr[target] , 1 + arr[target - square])
                 
         return arr[n]
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        h = len(mat)
+        w = len(mat[0])
+        q = []
+        
+        for i in range(h):
+            for j in range(w):
+                if mat[i][j] == 0:
+                    q.append((i,j))
+                else:
+                    mat[i][j] = "#"
+            
+        
+        for row,col in q:
+            for dx, dy in (1,0),(-1,0),(0,1),(0,-1):
+                nr = row + dx
+                nc = col + dy
+                
+                if 0 <= nr < h and 0 <= nc < w and mat[nr][nc] == "#":
+                    mat[nr][nc] = mat[row][col] + 1
+                    q.append((nr,nc))
+                    
+        return mat
