098460ead838cbc65ad05b55928127e7e03fef96
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-25 17:49:12 -0400

    Climbing Stairs
    You are climbing a staircase. It takes n steps to reach the top.
    
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?

--- recursion.py
+++ recursion.py
@@ -6,3 +6,12 @@ class Solution:
             return 1
         
         return self.fib(n-1) + self.fib(n-2)
+    def climbStairs(self, n: int) -> int:
+        one, two = 1,1
+        
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+            
+        return one
