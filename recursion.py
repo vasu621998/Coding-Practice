747993da377edc9070b1bf98791d93da4882f507
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-26 00:08:15 -0400

    POW 2

--- recursion.py
+++ recursion.py
@@ -30,3 +30,22 @@ class Solution:
         res = helper(x, abs(n))
         
         return res if n >= 0 else 1 / res
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        return self.helper(x, n)
+    
+    def helper(self, x, n):
+        if n == 0:
+            return 1
+        
+        partialAnswer = self.helper( x, n // 2 )
+        if n % 2 == 1:
+            partialAnswer = partialAnswer * partialAnswer * x
+        else:
+            partialAnswer = partialAnswer * partialAnswer
+        
+        return partialAnswer
+            
