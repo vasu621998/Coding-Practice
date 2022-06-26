ad048d5e9439445782550dd2edde8fc73bcd27ff
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-25 23:57:43 -0400

    Pow(x, n)
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).

--- recursion.py
+++ recursion.py
@@ -15,3 +15,18 @@ class Solution:
             two = temp
             
         return one
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x,n):
+            if x == 0:
+                return 0
+            if n == 1:
+                return x
+            
+            res = helper(x , n // 2)
+            res = res * res
+            
+            return x * res if  n % 2 else res
+            
+        res = helper(x, abs(n))
+        
+        return res if n >= 0 else 1 / res
