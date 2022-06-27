1a4ff99be0502f20c268962efd3d205a2fa2f8f1
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-27 08:25:14 -0400

    Sort an Array
    Given an array of integers nums, sort the array in ascending order.

---
+++ recursion2.py
@@ -0,0 +1,46 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        def merge_sort(arr):
+
+            if len(arr)<=1:
+                return arr
+
+            start = 0
+            end = len(arr)
+            mid = (start+end)//2
+
+            left = arr[:mid]
+            right = arr[mid:]
+
+            merge_sort(left)
+            merge_sort(right)
+
+            i = j = k = 0
+
+
+            while i < len(left) and j < len(right):
+
+                if left[i] <= right[j]:
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+
+            while i < len(left):
+                arr[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                arr[k] = right[j]
+                j += 1
+                k += 1
+                
+        merge_sort(nums)
+        return nums
+                    
+                
+        
