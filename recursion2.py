07c5f60d0e09602324b2106ba29d9ea1ebdeb97f
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-28 16:35:41 -0400

    N-Queens II
    The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
    
    Given an integer n, return the number of distinct solutions to the n-queens puzzle.

--- recursion2.py
+++ recursion2.py
@@ -41,6 +41,34 @@ class Solution:
                 
         merge_sort(nums)
         return nums
-                    
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        posDiag = set() # r + c
+        negDiag = set() # r - c
+        
+        
+        res = 0
+        def backtrack(r):
+            if r == n:
+                
+                nonlocal res
+                res += 1
+                return
+            
+            for c in range(n):
+                
+                if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                    continue
+                
+                col.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+                backtrack(r+1)                
+                col.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+                
+        backtrack(0)
+        return res                    
                 
         
