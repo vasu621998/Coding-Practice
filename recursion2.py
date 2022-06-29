477fa48c4ae8559813c31b3425c8622eae9f5c42
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-06-29 15:52:36 -0400

    Combinations
    Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
    
    You may return the answer in any order.

--- recursion2.py
+++ recursion2.py
@@ -70,5 +70,22 @@ class Solution:
                 
         backtrack(0)
         return res                    
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        
+        def backtrack(start, comb):
+            if len(comb) == k:
+                res.append(comb.copy())
+                return
+        
+            for i in range(start, n+1):
+                comb.append(i)
                 
+                backtrack(i + 1, comb)
+                
+                comb.pop()
+        
+        backtrack(1, [])
+        
+        return res                
         
