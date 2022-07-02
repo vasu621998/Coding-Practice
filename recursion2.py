a0176f3c3ad5ce86fca705af39617427e747a1e1
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-02 09:09:07 -0400

    Letter Combinations of a Phone Number
    Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
    
    A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.

--- recursion2.py
+++ recursion2.py
@@ -180,3 +180,25 @@ class Solution:
         for i,h in stack:
             maxArea = max(maxArea, h * (len(heights) - i))
         return maxArea
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+        def backTracking(i, currentStr):
+            if len(currentStr) == len(digits):
+                res.append(currentStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backTracking(i+1, currentStr+c)
+        if digits:
+            backTracking(0, "")
+        
+        return res
