61b38ea1d7d03fbbbc7f2432855e20fdd0444ef8
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-02 09:13:06 -0400

    The Skyline Problem
    A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
    
    The geometric information of each building is given in the array buildings where buildings[i] = [lefti, righti, heighti]:
    
    lefti is the x coordinate of the left edge of the ith building.
    righti is the x coordinate of the right edge of the ith building.
    heighti is the height of the ith building.
    You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
    
    The skyline should be represented as a list of "key points" sorted by their x-coordinate in the form [[x1,y1],[x2,y2],...]. Each key point is the left endpoint of some horizontal segment in the skyline except the last point in the list, which always has a y-coordinate 0 and is used to mark the skyline's termination where the rightmost building ends. Any ground between the leftmost and rightmost buildings should be part of the skyline's contour.
    
    Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...,[2 3],[4 5],[7 5],[11 5],[12 7],...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...,[2 3],[4 5],[12 7],...]

--- recursion2.py
+++ recursion2.py
@@ -202,3 +202,47 @@ class Solution:
             backTracking(0, "")
         
         return res
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        res = []
+        events = [] 
+        q = [0]
+        heapq.heapify(q)
+        left = []
+        right = []
+        
+        for b in buildings:
+            left.append([b[0], -b[-1], "start"])
+    
+        #1. Sort start edges by height from high -> low
+        left.sort(key = lambda x: x[1])
+    
+        for b in buildings:
+            right.append([b[1], -b[-1], "end"])
+        
+        #1. Sort end edges by height from low -> high
+        right.sort(key = lambda x: x[1], reverse=True)
+        
+        #1. Sort points by left edge
+        events = left + right
+        events.sort(key = lambda x: x[0])
+         
+        for e in events:
+            #2.  Get the start edges
+            if e[-1] == "start":
+                # If the current edge is higher than the current highest edges,
+                # add it to the queue 
+                if -e[1] > -heapq.nsmallest(1, q)[0]:
+                    res.append([e[0], -e[1]])
+                heapq.heappush(q, e[1])
+                
+            #3.  Get the end edges
+            if e[-1] == "end":
+                # Remove the height of this edge from the queue
+                q.remove(e[1])
+                
+                # If the height of this edge was the highest edges,
+                # add the position of this edge with the remain highest edges to the answer
+                if -e[1] > -heapq.nsmallest(1, q)[0]:
+                    res.append([e[0],-heapq.nsmallest(1, q)[0]])
+            
+        return res      
