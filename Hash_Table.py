7a33c61aed6c5ae984a9e02e0f92067391f98bce
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-04 23:43:19 -0400

    Design HashMap
    Design a HashMap without using any built-in hash table libraries.
    
    Implement the MyHashMap class:
    
    MyHashMap() initializes the object with an empty map.
    void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
    int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
    void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.

--- Hash_Table.py
+++ Hash_Table.py
@@ -24,3 +24,23 @@ class MyHashSet:
 # obj.add(key)
 # obj.remove(key)
 # param_3 = obj.contains(key)
+class MyHashMap:
+
+    def __init__(self):
+        self.l = [-1 for _ in range(1000001)]
+
+    def put(self, key: int, value: int) -> None:
+        self.l[key] = value
+
+    def get(self, key: int) -> int:
+        return self.l[key]
+
+    def remove(self, key: int) -> None:
+        self.l[key] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
