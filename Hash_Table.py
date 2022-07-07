d42b73ba85f7a75d8742d48518cccd08195de70e
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-07 09:28:27 -0400

    Minimum Index Sum of Two Lists
    Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
    
    You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.

--- Hash_Table.py
+++ Hash_Table.py
@@ -97,3 +97,24 @@ class Solution:
             ST[c1] = c2
             TS[c2] = c1
         return True
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        hashmap = {}
+        for i in range(len(list1)):   #step 1
+            hashmap[list1[i]] = i
+        
+        res = []
+
+        minsum = float("inf")   #step 2
+        
+        for j in range(len(list2)):    #step 3
+            if list2[j] in hashmap:
+                Sum = j + hashmap[list2[j]]    #step 3a
+                
+                if Sum < minsum:   #step 3b
+                    minsum = Sum
+                    res = []
+                    res.append(list2[j])
+                elif Sum == minsum:     #step 3c
+                    res.append(list2[j])
+        
+        return res
