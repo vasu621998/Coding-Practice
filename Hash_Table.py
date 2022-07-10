0a7d73645a6d0f6a5b0b4df81f749c325d541914
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-09 23:53:54 -0400

    4Sum II
    Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
    
    0 <= i, j, k, l < n
    nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0

--- Hash_Table.py
+++ Hash_Table.py
@@ -201,3 +201,21 @@ class Solution:
             cset.add(s[r])
             res  = max(res, r-l+1)
         return res
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        n=len(nums1)
+        res=0
+        d1=defaultdict(int)
+        d2=defaultdict(int)
+        
+        for i in range(n):
+            for j in range(n):
+                d1[nums1[i]+nums2[j]]+=1
+        
+        for i in range(n):
+            for j in range(n):
+                d2[nums3[i]+nums4[j]]+=1
+        
+        for key in d1:
+            res+=(d1[key]*d2[-key])
+        
+        return res
