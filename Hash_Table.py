1c952e0a928fa3d0c0b5621f3fc6ca66cd7087b7
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-10 18:21:50 -0400

    Insert Delete GetRandom O(1)
    Implement the RandomizedSet class:
    
    RandomizedSet() Initializes the RandomizedSet object.
    bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
    bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
    int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
    You must implement the functions of the class such that each function works in average O(1) time complexity.

--- Hash_Table.py
+++ Hash_Table.py
@@ -238,3 +238,37 @@ class Solution:
                 if len(res) ==k:
                     return res
         
+class RandomizedSet:
+
+
+    def __init__(self):
+        self.val_num = dict()
+        self.num_val = dict()
+        self.length = 0
+
+    def insert(self, val: int) -> bool:
+        if val in self.val_num:
+            return False
+        self.length += 1
+        self.val_num[val] = self.length
+        self.num_val[self.length] = val
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.val_num:
+            return False
+        num = self.val_num[val]
+        if num == self.length:
+            del self.val_num[val]
+            del self.num_val[num]
+        else:
+            last = self.num_val[self.length]
+            self.val_num[last] = num
+            del self.val_num[val]
+            self.num_val[num] = last
+            del self.num_val[self.length]
+        self.length -= 1
+        return True
+
+    def getRandom(self) -> int:
+        return self.num_val[randint(1, self.length)]
