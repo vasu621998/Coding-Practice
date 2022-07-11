89ccb3154b294ff677aa556477119087b9012cd2
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-11 14:09:57 -0400

    Product of Array Except Self
    Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
    
    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
    
    You must write an algorithm that runs in O(n) time and without using the division operation.

--- 75_Blind.py
+++ 75_Blind.py
@@ -25,3 +25,16 @@ class Solution:
                 return True
             hashset.add(i)
         return False
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = 1
+        res = [1] * n
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1        
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
