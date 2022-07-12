0371e4fdfd9b9ae2d60a29a50a43117d5d6ad758
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-12 11:36:44 -0400

    Search in Rotated Sorted Array
    There is an integer array nums sorted in ascending order (with distinct values).
    
    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
    
    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
    
    You must write an algorithm with O(log n) runtime complexity.

--- 75_Blind.py
+++ 75_Blind.py
@@ -61,3 +61,23 @@ class Solution:
             currMin = min( temp, n * currMin, n)
             res = max(res, currMax)
         return res
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0 , len(nums) - 1
+        
+        while l <= r :
+            mid = (l+r) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] >= nums[l]:
+                if nums[mid] < target or target < nums[l] :
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if nums[mid] > target or target > nums[r] :
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
