3a1563060c34bb45304d4e9f99ce19e0ab32fcfb
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-12 00:02:44 -0400

    Maximum Product Subarray
    Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
    
    The test cases are generated so that the answer will fit in a 32-bit integer.
    
    A subarray is a contiguous subsequence of the array.

--- 75_Blind.py
+++ 75_Blind.py
@@ -48,3 +48,16 @@ class Solution:
             currSum += n
             maxSub = max(maxSub, currSum)
         return maxSub
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        currMax, currMin = 1,1 
+        
+        for n in nums:
+            if n == 0:
+                currMax, currMin = 1,1 
+                continue
+            temp = n * currMax
+            currMax = max( n * currMax, n * currMin, n)
+            currMin = min( temp, n * currMin, n)
+            res = max(res, currMax)
+        return res
