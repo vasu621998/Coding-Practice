2855cd15aca60625c036543ac763f0c3a0184b3e
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-15 18:17:21 -0400

    Reverse Bits
    Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
    In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.

--- 75_Blind.py
+++ 75_Blind.py
@@ -169,3 +169,14 @@ class Solution:
         
         return res
     
+    def reverseBits(self, n: int) -> int:
+        res = 0
+
+        for i in range(32):
+    
+            res = res << 1
+            bit = n%2
+            res += bit
+            n = n >> 1
+
+        return res
