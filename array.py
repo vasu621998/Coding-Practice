ff2136297fefd0aebb84a8145497dd26fd9e20e6
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-18 10:27:31 -0400

    Best Time to Buy and Sell Stock with Cooldown
    You are given an array prices where prices[i] is the price of a given stock on the ith day.
    
    Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
    
    After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
    Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).

--- array.py
+++ array.py
@@ -328,3 +328,27 @@ class Solution:
                 high = mid
                 
         return False
+    def maxProfit(self, prices: List[int]) -> int:
+        #buying. = i + 1
+        # selling = i + 2
+        
+        dp = {} # key = (i, buying), val = profit
+        
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+            
+            if (i,buying) in dp:
+                return dp[(i, buying)]
+            
+            if buying:
+                buy = dfs(i+1, not buying) - prices[i]
+                cooldown = dfs(i+1, buying)
+                dp[(i,buying)] = max(buy,cooldown)
+            else:
+                sell = dfs(i+2, not buying) + prices[i]
+                cooldown = dfs(i+1, buying)
+                dp[(i,buying)] = max(sell,cooldown)
+            return dp[(i, buying)]
+        
+        return dfs(0, True)
