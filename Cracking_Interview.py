a2b55e8f647a930c14e2f63419ba2ac78abab649
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-20 23:33:14 -0400

    FIND LEAVES OF BINARY TREE
     FIND LEAVES OF BINARY TREE | PYTHON - POSTORDER DFS

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -12,3 +12,27 @@ class Solution:
                      que.extend(cur.getList())
               depth += 1
           return res
+    # T: O(N)
+    # S: O(N)
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = collections.defaultdict(list)
+        
+        def dfs(node, layer):
+            if not node:
+                return layer
+            
+            left = dfs(node.left, layer)
+            right = dfs(node.right, layer)
+            
+            layer = max(left, right)
+            
+            res[layer].append(node.val)
+            
+            return layer + 1
+    
+        dfs(root, 0)
+        
+        return res.values()
+    # T: O(N)
+    # S: O(N)
+    
