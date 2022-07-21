eeb616bdde2ca6840e0712cc7c8ac89a568ac4d8
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-21 08:46:26 -0400

    Binary Tree Right Side View
    Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -12,6 +12,7 @@ class Solution:
                      que.extend(cur.getList())
               depth += 1
           return res
+    # BFS
     # T: O(N)
     # S: O(N)
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
@@ -33,6 +34,33 @@ class Solution:
         dfs(root, 0)
         
         return res.values()
+    # DFS
     # T: O(N)
     # S: O(N)
-    
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        queue = collections.deque([root])
+        res = []
+        
+        while queue:
+            length = len(queue)
+            for i in range(length):
+                node = queue.popleft()
+
+                
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+                
+                
+                if i == length - 1:
+                    res.append(node.val)
+                    
+        return res  
+    # DFS
+    # T: O(N)
+    # S: O(N)
