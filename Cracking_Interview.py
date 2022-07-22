8f29f9a7c9382315122185289a08588a509415fc
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-22 08:55:31 -0400

    Valid Palindrome II
    Given a string s, return true if the s can be palindrome after deleting at most one character from it.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -212,3 +212,28 @@ class SparseVector:
         
 # T: O(2 * N) --> O(N)        
 # S: O(N)
+    def validPalindrome(self, s: str) -> bool:        
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            if s[l] != s[r]:
+                return self.is_palindrome(s, l+1, r) or self.is_palindrome(s, l, r -1)
+            l +=1
+            r -=1
+        return True
+    
+    
+    def is_palindrome(self, s, l, r):            
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+#Input: s = "abca"
+#Output: true
+#Explanation: You could delete the character 'c'.
+        
+# T: O(N)
+# S: O(1)
