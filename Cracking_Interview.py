9e0592cf0c741065f41aefd59b23a3197cd64d97
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-22 09:45:31 -0400

    Group Anagrams
    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
    
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -237,3 +237,20 @@ class SparseVector:
         
 # T: O(N)
 # S: O(1)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+                
+            res[tuple(count)].append(s)
+        return res.values()
+
+#Input: strs = ["eat","tea","tan","ate","nat","bat"]
+#Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        
+# T: O(N) * O(K) -> K = Longest string length
+# S: O(NK)
