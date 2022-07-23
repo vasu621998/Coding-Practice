b1da05ed4fd7fe5308322732882de5493e1d005b
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-23 14:37:51 -0400

    Letter Combinations of a Phone Number
    Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
    
    A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -446,3 +446,31 @@ class SparseVector:
 # Explanation: 342 + 465 = 807.        
 # T: O(N+M)
 # S: O(N+M)
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+        def backTracking(i, currentStr):
+            if len(currentStr) == len(digits):
+                res.append(currentStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backTracking(i+1, currentStr+c)
+        if digits:
+            backTracking(0, "")
+        
+        return res
+
+    
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]    
+# T: O(4^N * N)
+# S: O(N)
