9b8a7c31248e796311e86754b46a0883a88acedb
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-23 12:11:50 -0400

    Add Two Numbers
    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
    
    You may assume the two numbers do not contain any leading zero, except the number 0 itself.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -420,3 +420,29 @@ class SparseVector:
 
 #T: O(N)
 #S: O(1)
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            val = v1 + v2 + carry
+            
+            carry = val // 10
+            val = val % 10
+            
+            curr.next = ListNode(val)
+            
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.        
+# T: O(N+M)
+# S: O(N+M)
