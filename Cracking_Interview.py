10d21d6005034867fc6f191de1730dae536e9e54
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-24 14:19:42 -0400

    Remove All Adjacent Duplicates in String II
    You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
    
    We repeatedly make k duplicate removals on s until we no longer can.
    
    Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -707,3 +707,32 @@ class SparseVector:
 # Output: "ay"
 #T : O(N)
 #S: O(N)
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        
+        for char in s:
+            if not stack:
+                stack.append([char, 1])
+            
+            else:                
+                if(char != stack[-1][0]):
+                    stack.append([char, 1])
+                else:
+                    if stack[-1][1] + 1 == k:
+                        stack.pop()
+                    else:
+                        stack[-1][1] += 1
+        
+        stack = [char * count for char, count in stack]
+        return "".join(stack)
+                    
+#Input: s = "deeedbbcccbdaa", k = 3
+#Output: "aa"
+#Explanation: 
+#First delete "eee" and "ccc", get "ddbbbdaa"
+#Then delete "bbb", get "dddaa"
+#Finally delete "ddd", get "aa"
+        
+#T: O(N) + O(N) + O(N) --> O(N)
+#S: O(N)
+
