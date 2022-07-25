5e32c1f4f789bc95b7fe8f0cc55be154675724cb
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-25 12:23:46 -0400

    Longest Increasing Path in a Matrix
    Given an m x n integers matrix, return the length of the longest increasing path in matrix.
    
    From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -794,3 +794,36 @@ class SparseVector:
     
 #T : O(N)
 #S : O(1)
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        self.memo = {}
+        
+        self.directions = {(1,0), (0,1), (-1,0), (0,-1)}
+        
+        res = 1
+        
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                res = max(res, self.dfs(matrix, row, col))
+        
+        return res
+    def dfs(self, matrix, cur_row, cur_col):
+        if (cur_row, cur_col) in self.memo:
+            return self.memo[(cur_row, cur_col)]
+        
+        self.memo[(cur_row, cur_col)] = 1
+        
+        for row_inc,col_inc in self.directions:
+            new_row = cur_row + row_inc
+            new_col = cur_col + col_inc
+            
+            if (0 <= new_row < len(matrix)) and (0<= new_col < len(matrix[0])) and matrix[cur_row][cur_col] < matrix[new_row][new_col]:
+                self.memo[(cur_row,cur_col)] = max(self.memo[(cur_row,cur_col)] , 1 + self.dfs(matrix, new_row, new_col))
+                
+                
+        return self.memo[(cur_row, cur_col)]
+    
+# Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+#Output: 4
+#Explanation: The longest increasing path is [1, 2, 6, 9].
+# T: O(R * C)                
+# S: O(R * C)  
