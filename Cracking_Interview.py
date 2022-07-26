067819d18675f250438c4f7121e38d44f127a40c
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-26 19:06:32 -0400

    Longest Valid Parentheses
    Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1038,3 +1038,41 @@ class SparseVector:
 
 # T: O(N)
 # S: O(N)
+    def longestValidParentheses(self, s: str) -> int:
+        l_count = r_count = max_len = 0
+        i = 0
+        while i< len(s):
+            if s[i] == "(":
+                l_count += 1
+            else:
+                r_count += 1
+                
+            if l_count == r_count:
+                max_len = max(max_len, l_count + r_count)
+            elif r_count > l_count:
+                l_count = r_count = 0
+            
+            i += 1
+        l_count = r_count = 0 
+        i = len(s) - 1
+        
+        while i >= 0:
+            if s[i] == "(":
+                l_count += 1
+            else:
+                r_count += 1
+                
+            if l_count == r_count:
+                max_len = max(max_len, l_count + r_count)
+            elif l_count > r_count:
+                l_count = r_count = 0            
+            
+            i -= 1
+            
+        return max_len
+
+# Input: s = "(()"
+# Output: 2
+# Explanation: The longest valid parentheses substring is "()".      
+# T: O(N) + O(N) -> O(N)
+# S: O(1)
