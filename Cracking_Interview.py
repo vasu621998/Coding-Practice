941bde3c2b24de50f1ed55509cea4f55ba107c6d
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-27 13:28:34 -0400

    Pow(x, n)
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1189,3 +1189,18 @@ def count_substring(string,sub_string):
 
 # T: O(N)        
 # S: O(N)
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x,n):
+            if x == 0: return 1
+            if n == 1: return x
+            res = helper(x , n // 2)
+            res = res * res
+            return x * res if n % 2 else res
+        
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1/res
+
+#  Input: x = 2.00000, n = 10
+# Output: 1024.00000  
+# T : O(logN)    
+# S : O(1)
