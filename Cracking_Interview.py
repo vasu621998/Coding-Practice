ff31ef953dfb3fc4b3e55130f903b8d2b6de1864
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-26 21:40:11 -0400

    Find N Unique Integers Sum up to Zero
    Given an integer n, return any array containing n unique integers such that they add up to 0.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1076,3 +1076,25 @@ class SparseVector:
 # Explanation: The longest valid parentheses substring is "()".      
 # T: O(N) + O(N) -> O(N)
 # S: O(1)
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+        
+        if not n:
+            return res
+        
+        if n % 2:
+            res.append(0)
+            
+        
+        for i in range(1, n//2 + 1):
+            res.append(i)
+            res.append(-i)
+            
+        return res
+
+# Input: n = 5
+# Output: [-7,-1,1,3,4]
+# Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].    
+    
+#T: O(N//2) -> O(N)
+#S: O(N)
