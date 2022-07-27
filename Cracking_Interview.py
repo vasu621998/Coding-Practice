30676c0e5d7add043d80dc2acc99629bbaef4f42
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-27 17:26:43 -0400

    Minimum Area Rectangle
    You are given an array of points in the X-Y plane points where points[i] = [xi, yi].
    
    Return the minimum area of a rectangle formed from these points, with sides parallel to the X and Y axes. If there is not any such rectangle, return 0.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1228,3 +1228,23 @@ def count_substring(string,sub_string):
 
 # T: O(N)
 # S: O(1)
+    def minAreaRect(self, points: List[List[int]]) -> int:
+        min_size = float('inf')
+        visited = set()
+        
+        
+        for x1, y1 in points:
+            for x2, y2 in visited:
+                if (x1, y2) in visited and (x2, y1) in visited:
+                    size = abs(x2 - x1) * abs(y2 - y1)
+                    
+                    min_size = min(size, min_size)
+                    
+            visited.add((x1, y1))
+                
+        return min_size if min_size != float('inf') else 0
+    
+# Input: points = [[1,1],[1,3],[3,1],[3,3],[2,2]]
+# Output: 4
+# T: O(N^2)
+# S: O(N)
