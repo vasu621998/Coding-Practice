c18f2583ec50d67a7a19afbdf3356e4abe70ea0d
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-28 10:31:19 -0400

    Middle of the Linked List
    Given the head of a singly linked list, return the middle node of the linked list.
    
    If there are two middle nodes, return the second middle node.

--- LinkedList.py
+++ LinkedList.py
@@ -534,3 +534,18 @@ class DoublyLinkedList:
             prev = curr
             curr = nextpair
         return dummy.next
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
+    
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+# T: O(N)
+#S: O(1)
