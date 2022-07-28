0f3c7cd548130d4966dc24bd2aa109d0d031d944
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-28 11:34:42 -0400

    Binary Search
    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
    
    You must write an algorithm with O(log n) runtime complexity.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1330,3 +1330,27 @@ def count_substring(string,sub_string):
 
 # T: O(N)
 # S: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+                
+        return -1
+                
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+
+# T: O(logN)            
+# S: O(1)
