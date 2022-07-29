0a4b155a18601b08c9555f769806157b5e0577a7
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-29 10:20:24 -0400

    Invert Binary Tree
    Given the root of a binary tree, invert the tree, and return its root.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1371,3 +1371,18 @@ def count_substring(string,sub_string):
 
 # T: O(N)
 # S: O(N)
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+        
+# T : O(N)
+# S : O(N)
