20e0a0839f854518ee9a2f28885bdacd00a0899f
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-29 16:28:49 -0400

    Balanced Binary Tree
    Given a binary tree, determine if it is height-balanced.
    
    For this problem, a height-balanced binary tree is defined as:
    
    a binary tree in which the left and right subtrees of every node differ in height by no more than 1.

--- Cracking_Interview.py
+++ Cracking_Interview.py
@@ -1423,3 +1423,21 @@ def count_substring(string,sub_string):
 
 # T: O(N)
 # S: O(1)
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root):
+            if not root:
+                return [True, 0]
+            
+            left, right = dfs(root.left), dfs(root.right)
+            
+            balanced = (left[0] and right[0] and 
+                        abs(left[1] - right[1]) <= 1)
+            
+            return [balanced, 1 + max(left[1], right[1])]
+        
+        
+        return dfs(root)[0]
+    
+# T: O(N)
+# S: O(1)
