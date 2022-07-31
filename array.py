dc13c62671a2a4e62463397eec75865c926c6023
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-07-31 14:13:06 -0400

    How Many Numbers Are Smaller Than the Current Number
    Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
    
    Return the answer in an array.

--- array.py
+++ array.py
@@ -518,3 +518,21 @@ class Solution:
 
 # T: O(N) + O(N)
 # S: O(N)
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        temp = nums[:]
+        
+        temp.sort()
+        
+        return [temp.index(n) for n in nums]
+    
+# Input: nums = [8,1,2,2,3]
+# Output: [4,0,1,1,3]
+# Explanation: 
+# For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+# For nums[1]=1 does not exist any smaller number than it.
+# For nums[2]=2 there exist one smaller number than it (1). 
+# For nums[3]=2 there exist one smaller number than it (1). 
+# For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+    
+# T:O(N)
+# S:O(N)
