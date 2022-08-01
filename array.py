0e3afa63d20b835f1cf94052277f247973b122a0
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-01 11:24:09 -0400

    Unique Paths
    There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
    
    Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
    
    The test cases are generated so that the answer will be less than or equal to 2 * 109.

--- array.py
+++ array.py
@@ -560,4 +560,23 @@ class Solution:
 # Input: n = 1
 # Output: true
 # Explanation: 20 = 1
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        
+        for i in range(m -1):
+            newRow = [1] * n
+            for j in range(n-2, -1, -1):
+                newRow[j] = newRow[j+1] + row[j]
+            
+            row = newRow
+        return row[0]
 
+#Input: m = 3, n = 2
+#Output: 3
+#Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+#1. Right -> Down -> Down
+#2. Down -> Down -> Right
+#3. Down -> Right -> Down
+    
+#T: O(M+N)
+#S: O(N)
