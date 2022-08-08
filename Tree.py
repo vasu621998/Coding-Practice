16c33e162bb1f2aad9b5225c039231bfa257c97b
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-07 21:42:22 -0400

    Binary Tree Paths
    Given the root of a binary tree, return all root-to-leaf paths in any order.
    
    A leaf is a node with no children.

--- Tree.py
+++ Tree.py
@@ -146,3 +146,28 @@ class Solution:
 
 # T: O(N)
 # S: O(N)
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # DFS solution
+        output = []
+        stack = [(root, '')]
+        
+        while stack:
+            node, path = stack.pop()
+            path += str(node.val)
+            
+            if not node.left and not node.right:
+                output.append(path)
+                
+            path += '->'
+            if node.left:
+                stack.append((node.left, path))
+            if node.right:
+                stack.append((node.right, path))
+                
+        return output    
+
+#Input: root = [1,2,3,null,5]
+#Output: ["1->2->5","1->3"]
+    
+# T: O(N)
+# S: O(N)
