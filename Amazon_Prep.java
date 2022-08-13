6a256fe573a5223d98e765edd4977c3213564fbd
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:17:42 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -475,3 +475,14 @@ Array.Sort(parcels);
 
         return maxLength;
     }
+
+
+import heapq
+def ConnectRope(ropes):
+    heapq.heapify(ropes)
+    res = 0
+    while len(ropes) > 1:
+        cur = heapq.heappop(ropes)+heapq.heappop(ropes)
+        heapq.heappush(ropes, cur)
+        res += cur
+    return res
