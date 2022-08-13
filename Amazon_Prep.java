220af35d8ef77f37c4939ae6500265a18f755754
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 10:19:46 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -988,3 +988,37 @@ def password(word):
 		}
 		return count;
     }
+
+	
+// Question 1
+//Maximum Avarage Subtree (In The Name Of Employee Tree)
+//Solution:
+//Java bottom-up naive recursion solution with complexity O(N).
+	
+public class Solution {
+    public class Result{
+    int sum  , num;
+    Result(int sum , int num){
+        this.sum = sum;
+        this.num = num;
+    }
+}
+TreeNode maxNode = null;
+Result maxResult = null;
+    public TreeNode findSubtree2(TreeNode root) {
+        MaxFinder(root);
+        return maxNode;
+    }
+    public Result MaxFinder(TreeNode root){
+        if(root == null)
+            return new Result(0 , 0);
+        Result left = MaxFinder(root.left);
+        Result right = MaxFinder(root.right);
+        Result rootResult= new Result(left.sum+ right.sum + root.val , left.num + right.num + 1);
+        if(maxNode == null || maxResult.sum*rootResult.num<maxResult.num*rootResult.sum){
+            maxResult = rootResult;
+            maxNode =  root;
+        }
+        return rootResult;
+}
+}
