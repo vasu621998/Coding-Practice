42d501e600a9cff13698acd6b1c51e3ad80054f5
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:36:05 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -621,3 +621,17 @@ public List<Integer> numberOfItems(String s, List<Integer> startIndices, List<In
         }
         return ans;
     }
+
+	
+    def countBinarySubstrings(self, s: str) -> int:
+        prev , curr , res = 0 , 1 , 0
+        
+        for i in range(1,len(s)):
+            if s[i-1] == s[i]:
+                curr +=1
+            else:
+                prev = curr
+                curr = 1
+            if prev >= curr:
+                res+=1
+        return res   
