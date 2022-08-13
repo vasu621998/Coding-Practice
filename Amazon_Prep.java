2142be76a2755484afc42464556ba1a5c993a907
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:29:38 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -575,3 +575,21 @@ private static void alignedGroups(int[][] isConnected, boolean[] isReached, int 
     }
 
 }
+
+	
+	
+public static String simpleCipher(String encrypted, int k){
+        char[] _encrypted = encrypted.toCharArray();
+        for(int i=0; i < encrypted.length(); i++){
+            char x = _encrypted[i];
+            // if the previous kth element is greater than 'A'
+            if(x-k>=65){
+                _encrypted[i] = (char)(x-k);
+            }
+            //if ascii code of kth previous element if less than that of A add 26 to it
+            else{
+                _encrypted[i] = (char)(x-k+26);
+            }
+        }
+        return new String(_encrypted);
+    }
