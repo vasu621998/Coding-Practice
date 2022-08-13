f5261ce603b987e04bd00fce0b4b0edb060cbf35
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:19:44 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -486,3 +486,50 @@ def ConnectRope(ropes):
         heapq.heappush(ropes, cur)
         res += cur
     return res
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AmazonsumClosest
+{
+
+public static int [] test(int [] nums, int d)
+{
+	int [] result = new int [2];
+	int max = Integer.MIN_VALUE;
+	int start = 0;
+	int end = nums.length-1;
+	Arrays.sort(nums);
+	int i = 0;
+	int j = 0;
+	ArrayList<Integer> a = new ArrayList<Integer>();
+	
+	while(start <= end)
+	{
+		if((nums[start] + nums[end])<= d-30)
+		{
+			
+			if(max < (nums[start] + nums[end]))
+			{
+				max = (nums[start] + nums[end]);
+				i = nums[start];
+				j = nums[end];
+			}
+			start++;
+		
+		}
+		else if((nums[start] + nums[end]) > d-30)
+				{
+					end--;
+				}
+	}
+	result[0] = i;
+	result[1] = j;
+// a.add(i);
+// a.add(j);
+// System.out.println(a);
+return result;
+}
+
