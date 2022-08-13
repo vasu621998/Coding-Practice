ad317cccebaeba8b47cba82c0fdaa3d8f3ccdc0a
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:34:12 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -593,3 +593,31 @@ public static String simpleCipher(String encrypted, int k){
         }
         return new String(_encrypted);
     }
+
+	
+public List<Integer> numberOfItems(String s, List<Integer> startIndices, List<Integer> endIndices) {
+        int n = s.length();
+        int[] dp = new int[n];
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '|') {
+                dp[i] = count;
+            } else {
+                count++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < startIndices.size(); i++) {
+            int start = startIndices.get(i);
+            int end = endIndices.get(i);
+
+            while (s.charAt(start) != '|') start++;
+            while (s.charAt(end) != '|') end--;
+            if (start < end) {
+                ans.add(dp[end] - dp[start]);
+            } else {
+                ans.add(0);
+            }
+        }
+        return ans;
+    }
