34f36ed337174aaaf93a46f387664defc70c06e2
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:16:35 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -433,3 +433,45 @@ Array.Sort(parcels);
 
         return minDays;
     }
+
+
+
+    public int maxLengthValidSubArray(int[] processingPower, int[] bootingPower, int maxPower){
+        if(processingPower == null || bootingPower == null 
+            || processingPower.length == 0 || processingPower.length != bootingPower.length){
+                return 0;
+        }
+
+        PriorityQueue<Integer> maxBootingPower = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        int maxLength = 0;
+        int currentLength = 1;
+
+        int start = 0;
+        int end = 0;
+        
+        int currentSumProcessingPower = processingPower[0];
+        maxBootingPower.add(bootingPower[0]);
+        while(end < processingPower.length){
+            int currentBootingPower = maxBootingPower.peek();
+            int currentPower = currentBootingPower + currentSumProcessingPower * currentLength;
+
+            if(currentPower <= maxPower){
+                maxLength = currentLength;
+                end++;
+                currentLength++;
+            }
+            else{
+                currentSumProcessingPower -= processingPower[start];
+                maxBootingPower.remove(bootingPower[start]);
+                start++;
+                end++;
+            }
+
+            if(end < processingPower.length){
+                maxBootingPower.add(bootingPower[end]);
+                currentSumProcessingPower += processingPower[end];
+            }
+        }
+
+        return maxLength;
+    }
