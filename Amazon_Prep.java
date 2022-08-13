1397fe4bb8f7c93fbc9b3dd128e9337d891d206f
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 10:04:12 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -912,3 +912,26 @@ cost += temp;
 return cost;
 }
 }
+
+	
+def MaxBoundedArray(n, low, up):
+	total = (up - low) * 2 + 1
+	if (n > total or up < low or n < 3):
+		return None
+	# if above check is passed, there definitely exists a solution
+	q = collections.deque()
+	# create the decreasing part
+	for ele in range(up, low-1, -1):
+		q.append(ele)
+	# case 1: decreasing part has length >= n
+	if len(q) >= n:
+		while len(q) > n-1:
+			q.pop()
+		q.appendleft(up-1)
+		return list(q)
+	# case 2: decreasing part has length < n, so now we can just append left elements
+	else:
+		for ele in range(up-1, low-1, -1):
+			q.appendleft(ele)
+			if len(q) == n:
+				return list(q)
