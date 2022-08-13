2330b4dd538242e84209c3b5fbbabe47f21b5f53
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-13 09:10:05 -0400

    Update Amazon_Prep.java

--- Amazon_Prep.java
+++ Amazon_Prep.java
@@ -325,3 +325,44 @@ class HelloWorld {
         return ans;
     }
 }
+
+
+public static int demolitionRobot(int[][]grid)
+{
+int n = grid.length;
+int m = grid[0].length;
+
+Queue<int[]> q = new LinkedList<>();
+boolean[][] visited = new boolean [n][m];
+
+int minD = Integer.MAX_VALUE;
+
+int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1, 0}};
+
+q.add(new int []{0,0});
+visited[0][0] =true;
+while(!q.isEmpty())
+{
+    int[] cur = q.remove();
+    for(int[] d : directions)
+    {
+        int nX = cur[0]+d[0];
+        int nY = cur[1]+d[1];
+        
+        if(nX<0|| nY<0 || nX>=n || nY>=m || grid[nX][nY]==0)
+            continue;
+        
+        if(grid[nX][nY]==9)
+            minD = Math.min(minD, grid[cur[0]][cur[1]]);
+        
+        if(grid[nX][nY]==1 && !visited[nX][nY])
+        {
+            grid[nX][nY]=  grid[cur[0]][cur[1]]+1;
+            visited[nX][nY]= true;
+            q.add(new int[]{nX, nY});
+        }
+    }
+}
+
+return minD;
+}
