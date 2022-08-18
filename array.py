24fc0f31847a3a82b746e38e6a481462553ec9c2
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-17 23:02:59 -0400

    Update array.py

--- array.py
+++ array.py
@@ -760,3 +760,20 @@ class Solution:
                             l += 1
                   
         return res 
+    def minSetSize(self, arr: List[int]) -> int:
+        d = {}
+        for x in arr:
+            if x not in d:
+                d[x] = 1
+            else:
+                d[x] += 1
+                  
+        l = sorted(d.values())
+        N = len(arr) // 2
+        idx = 0
+        
+        while N > 0:
+            N -= l[-idx-1]
+            idx += 1
+            
+        return idx 
