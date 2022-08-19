bba012b797673be19073397aa6ba2ed4ec36a379
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-19 08:22:20 -0400

    Update array.py

--- array.py
+++ array.py
@@ -777,3 +777,31 @@ class Solution:
             idx += 1
             
         return idx 
+	def isPossible(self, nums):
+		count = Counter(nums)
+		numLen = len(nums)
+
+		lastSub = defaultdict(int)
+
+		for num in nums:
+			
+			if not count[num]:
+				continue
+
+			count[num] -= 1
+
+			# append to existing sub list
+			if lastSub[num - 1]:
+				lastSub[num - 1] -= 1
+				lastSub[num] += 1
+
+			# add new sub list
+			elif count.get(num + 1) and count.get(num + 2):
+				count[num + 1] -= 1
+				count[num + 2] -= 1
+				lastSub[num + 2] += 1
+
+			else:
+				return False
+
+		return True
