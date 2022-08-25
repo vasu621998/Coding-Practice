6226c04345f890c070341699370568ff25a06833
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-25 14:48:07 -0400

    Ransom Note
    Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
    
    Each letter in magazine can only be used once in ransomNote.

--- string.py
+++ string.py
@@ -344,3 +344,11 @@ class Solution:
         return len(res) 
 # T: O(N)
 # S: O(1)
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote=list(ransomNote)
+        magazine=list(magazine)
+        for i in ransomNote:
+            if i in magazine:
+                magazine.remove(i)
+            else:return False
+        return True  
