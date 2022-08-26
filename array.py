091b48fe326d15c0a90a45d3e194718a616c2e34
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-25 21:48:51 -0400

    Update array.py

--- array.py
+++ array.py
@@ -814,3 +814,10 @@ class Solution:
             division = division/4
 
         return division == 1
+    def reorderedPowerOf2(self, n: int) -> bool:
+        n1 = sorted(str(n))
+    
+        for i in range(30):
+            res = sorted(str(2 ** i))
+            if res == n1:
+                return True
