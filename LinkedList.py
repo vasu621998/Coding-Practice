ce9a3d528acd17fd946f364ac59ec1bbe519290c
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-08-29 07:45:35 -0400

    Reverse Nodes in k-Group
    Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
    
    k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
    
    You may not alter the values in the list's nodes, only nodes themselves may be changed.

--- LinkedList.py
+++ LinkedList.py
@@ -581,3 +581,49 @@ class DoublyLinkedList:
     
 # T: O(N)
 # S: O(1)
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+	
+		#even if n (number of nodes) == 1 than k will be also 1
+        if k == 1:
+            return head
+        
+		#two pointers to get head and tails
+        last: Optional[ListNode] = head
+        first: Optional[ListNode] = head
+		
+		#steps to get the `k` pair we want
+        steps = 1
+        while steps < k:
+            if last == None:
+                return head
+            last = last.next
+            steps += 1
+            
+		# returning None shows our pair is incomplete and we don't have to reverse the pairs
+        if last == None:
+            return head
+			
+		# 'cause our last node will be head when reversed
+        head = last
+		
+		# reverse returns the first node (which will be the last node of the pair)
+		# and that last node of the pair is connected to next pair which will be sent by `reverseKGroup`
+        self.reverse(first,last).next = self.reverseKGroup(last.next,k)
+		
+        return head 
+            
+    def reverse(self,first: Optional[ListNode],last: Optional[ListNode]):
+        
+        # to reverse
+        prev: Optional[ListNode] = None
+        current: Optional[ListNode] = first
+        last: Optional[ListNode] = last.next
+        temp: Optional[ListNode]
+            
+        while (current != last):
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        
+        return first
