ba501cc97348fd20761c3fbd4b38052cb6f57d89
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-12 10:48:30 -0400

    Reorder List
    You are given the head of a singly linked-list. The list can be represented as:
    
    L0 → L1 → … → Ln - 1 → Ln
    Reorder the list to be on the following form:
    
    L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
    You may not modify the values in the list's nodes. Only nodes themselves may be changed.

--- LinkedList.py
+++ LinkedList.py
@@ -627,3 +627,34 @@ class DoublyLinkedList:
             current = temp
         
         return first
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        fast, slow = head.next, head
+        while fast and fast.next: 
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
+        
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+            
+        first, second = head, prev
+        
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+#T: O(N)
+#S: O(1)
+
+#Input: head = [1,2,3,4,5]
+#Output: [1,5,2,4,3]
