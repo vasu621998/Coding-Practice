c04b28501a0071a9e2e9caa34b8734cae314e6a4
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-13 12:08:45 -0400

    Maximum Binary Tree
    You are given an integer array nums with no duplicates. A maximum binary tree can be built recursively from nums using the following algorithm:
    
    Create a root node whose value is the maximum value in nums.
    Recursively build the left subtree on the subarray prefix to the left of the maximum value.
    Recursively build the right subtree on the subarray suffix to the right of the maximum value.
    Return the maximum binary tree built from nums.

--- Tree.py
+++ Tree.py
@@ -387,3 +387,34 @@ class Solution:
 
 # Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
 # Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        def maxTree(nums):
+            idx=nums.index(max(nums))
+            node = TreeNode(nums[idx])
+            if len(nums[idx+1:]) > 0:
+                node.right = maxTree(nums[idx+1:])
+            if len(nums[:idx]) > 0:
+                node.left = maxTree(nums[:idx])
+            return node
+        return maxTree(nums)
+
+# T: O(N)
+# S: O(1)
+
+
+#Input: nums = [3,2,1,6,0,5]
+#Output: [6,3,5,null,2,0,null,null,1]
+#Explanation: The recursive calls are as follow:
+#- The largest value in [3,2,1,6,0,5] is 6. Left prefix is [3,2,1] and right suffix is #[0,5].
+    #- The largest value in [3,2,1] is 3. Left prefix is [] and right suffix is [2,1].
+    #    - Empty array, so no child.
+    #    - The largest value in [2,1] is 2. Left prefix is [] and right suffix is [1].
+    #        - Empty array, so no child.
+    #        - Only one element, so child is a node with value 1.
+    #- The largest value in [0,5] is 5. Left prefix is [0] and right suffix is [].
+        #- Only one element, so child is a node with value 0.
+        #- Empty array, so no child.
