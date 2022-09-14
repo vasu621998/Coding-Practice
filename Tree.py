691ae6fbe4306b985c29870312fe3fb1acd869f5
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-14 08:20:44 -0400

    Pseudo-Palindromic Paths in a Binary Tree
    Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
    
    Return the number of pseudo-palindromic paths going from the root node to leaf nodes.

--- Tree.py
+++ Tree.py
@@ -418,3 +418,36 @@ class Solution:
     #- The largest value in [0,5] is 5. Left prefix is [0] and right suffix is [].
         #- Only one element, so child is a node with value 0.
         #- Empty array, so no child.
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        count = 0
+        # root = [2,3,1,3,1,null,1]
+
+        stack = [(root, 0)]
+        # path = 00000000
+        # nade = 2
+        # 1 << node.val = 1 << 2 = 00000100
+        # path = 00000100
+        # node = 3
+        # 1 << node.val = 00001000
+        # path = 00001100
+        # node =3
+        # 1 << node.val = 00001000
+        # path = 00000100
+        # path & (path -1)
+        # path -1 = 00000011
+        # count =1
+
+        while stack:
+            node, path = stack.pop()
+            if node is not None:
+                path = path ^ (1 << node.val)
+                # if it's a leaf, check if the path is pseudo-palindromic
+                if node.left is None and node.right is None:
+                    # check if at most one digit has an odd frequency
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:
+                    stack.append((node.left, path))
+                    stack.append((node.right, path))
+
+        return count
