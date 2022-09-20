75afcb9439e4a591b7e091a1f8a44deca757e61f
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-19 20:53:17 -0400

    Maximum Length of Repeated Subarray
    Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.

--- array.py
+++ array.py
@@ -1036,3 +1036,14 @@ class Solution:
         return i
 # T: O(N)
 # S: O(1)
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        strnum2 = ''.join([chr(x) for x in nums2])
+        strmax = ''
+        ans = 0
+        for num in nums1:
+            strmax += chr(num)
+            if strmax in strnum2:
+                ans = max(ans,len(strmax))
+            else:
+                strmax = strmax[1:]
+        return ans 
