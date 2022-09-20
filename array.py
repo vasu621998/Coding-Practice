1db547e809eb110ff8259d2af3cbc16926237b51
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-20 15:13:23 -0400

    59. Spiral Matrix II
    Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.

--- array.py
+++ array.py
@@ -1126,3 +1126,41 @@ class Solution:
 # Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 # Output: [[1,2],[3,10],[12,16]]
 # Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        r1 = 0
+        r2 = n -1 
+        c1 = 0
+        c2 = n -1
+        
+        res = [[0 for i in range(n)] for i in range(n)]
+        print(res)
+        k = 1
+        while(r1<=r2 and c1 <= c2):
+            for i in range(r1,r2+1):
+                res[r1][i] = k
+                k+=1
+
+            for i in range(c1+1,c2+1):
+                res[i][c2]=k
+                k+=1
+
+            for i in range(r2-1,r1-1,-1):
+                res[c2][i] = k
+                k+=1
+
+            for i in range(c2-1,c1,-1):
+                res[i][c1] = k
+                k+=1
+
+            r1+=1
+            c1+=1
+            c2-=1
+            r2-=1
+            
+        return res
+    
+# T: O(N)
+# S: O(1)
+
+# Input: n = 3
+# Output: [[1,2,3],[8,9,4],[7,6,5]]
