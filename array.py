339b0327b6bd13c8670b1044172ac7318eaa6982
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-20 11:54:36 -0400

    Jump Game
    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
    
    Return true if you can reach the last index, or false otherwise.

--- array.py
+++ array.py
@@ -1075,3 +1075,16 @@ class Solution:
                 board[r][c] = "."
         backtracking(0)
         return res 
+    def canJump(self, nums: List[int]) -> bool:
+        flag=True
+        n = len(nums)
+        for i in range(n):
+            if nums[i]==0 and i!=n-1:
+                flag=False
+                for j in range(i-1,-1,-1):
+                    if nums[j]>=i-j+1:
+                        flag=True
+                        break
+            if not flag:
+                return False
+        return flag 
