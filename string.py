6c14c05c267b56256396f30c8c3edefce2085396
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-23 09:05:28 -0400

    2278. Percentage of Letter in String
    Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.

--- string.py
+++ string.py
@@ -368,3 +368,17 @@ class Solution:
             res = temp
             
         return res
+    def percentageLetter(self, s: str, letter: str) -> int:
+        count = 0
+        for ele in s:
+            if ele==letter:
+                count+=1
+        return count*100//len(s)        
+
+# T: O(N*K)
+# S: O(1)
+
+#Input: s = "foobar", letter = "o"
+#Output: 33
+#Explanation:
+#The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.
