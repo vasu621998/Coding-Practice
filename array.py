d7c9952421cc32f002eccc579a944248acd1bb06
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-24 12:22:42 -0400

    78. Subsets
    Given an integer array nums of unique elements, return all possible subsets (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.

--- array.py
+++ array.py
@@ -1226,3 +1226,26 @@ class Solution:
         
         dfs(root,0,[])
         return list(ans) 
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        subset = []
+        
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i + 1)
+            
+            subset.pop()
+            dfs(i + 1)
+            
+        dfs(0)
+        return res
+    
+# T : O(N * 2^N)
+# S : O(1)
+
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
