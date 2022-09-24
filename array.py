a150f6068a75714ae283c67c991e2ccc6728314f
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-24 15:33:20 -0400

    72. Edit Distance
    Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
    
    You have the following three operations permitted on a word:
    
    Insert a character
    Delete a character
    Replace a character

--- array.py
+++ array.py
@@ -1276,3 +1276,34 @@ class Solution:
 
 # Input: nums = [2,0,2,1,1,0]
 # Output: [0,0,1,1,2,2]
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        
+        for i in range(1,n+1):
+            dp[i][0] = i
+
+        for j in range(1,m+1):
+            dp[0][j] = j
+            
+            
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                    
+                else:
+                    dp[i][j] = 1+ min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+                    
+        return dp[-1][-1]        
+    
+# T: O(N)
+# S: O(1)
+
+# Input: word1 = "horse", word2 = "ros"
+# Output: 3
+# Explanation: 
+# horse -> rorse (replace 'h' with 'r')
+# rorse -> rose (remove 'r')
+# rose -> ros (remove 'e')
