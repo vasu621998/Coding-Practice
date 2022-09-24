c8578ea5ead9b0af4dc92d3a50820516a01bba3a
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-24 16:52:30 -0400

    76. Minimum Window Substring
    Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
    
    The testcases will be generated such that the answer is unique.
    
    A substring is a contiguous sequence of characters within the string.

--- string.py
+++ string.py
@@ -382,3 +382,30 @@ class Solution:
 #Output: 33
 #Explanation:
 #The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.
+    def minWindow(self, s: str, t: str) -> str:
+        counter_t, window = Counter(t), defaultdict(int)
+        start = balance = 0
+        res = [len(s), 2 * len(s)]
+
+        for i, ch in enumerate(s):
+            if ch in counter_t:
+                window[ch] += 1
+                if window[ch] <= counter_t[ch]:
+                    balance += 1
+                if balance < len(t):
+                    continue
+                while s[start] not in counter_t or window[s[start]] > counter_t[s[start]]:
+                    if s[start] in counter_t:
+                        window[s[start]] -= 1
+                    start += 1
+                if i + 1 - start <= res[1] - res[0]:
+                    res = [start, i + 1]
+        
+        return s[res[0]:res[1]]    
+    
+# T : O(S + T)
+# S : O(T)
+
+# Input: s = "ADOBECODEBANC", t = "ABC"
+# Output: "BANC"
+# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
