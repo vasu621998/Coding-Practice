95495bcd9dbf167aab58e41a9351483a7280095b
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-26 21:41:22 -0400

    838. Push Dominoes
    There are n dominoes in a line, and we place each domino vertically upright. In the beginning, we simultaneously push some of the dominoes either to the left or to the right.
    
    After each second, each domino that is falling to the left pushes the adjacent domino on the left. Similarly, the dominoes falling to the right push their adjacent dominoes standing on the right.
    
    When a vertical domino has dominoes falling on it from both sides, it stays still due to the balance of the forces.
    
    For the purposes of this question, we will consider that a falling domino expends no additional force to a falling or already fallen domino.
    
    You are given a string dominoes representing the initial state where:
    
    dominoes[i] = 'L', if the ith domino has been pushed to the left,
    dominoes[i] = 'R', if the ith domino has been pushed to the right, and
    dominoes[i] = '.', if the ith domino has not been pushed.
    Return a string representing the final state.

--- Queue_BFS.py
+++ Queue_BFS.py
@@ -146,3 +146,29 @@ class MyCircularQueue:
                     q.append((nr,nc))
                     
         return mat
+    def pushDominoes(self, dominoes: str) -> str:
+        dominoes = list(dominoes)
+        active = set([i for i, d in enumerate(dominoes) if d != '.'])
+        while active:
+            nxt_active = set()
+            for i in active:
+                if dominoes[i] == 'L':
+                    if i > 0:
+                        if dominoes[i-1] == '.':
+                            dominoes[i-1] = 'L'
+                            nxt_active.add(i-1)
+                        elif dominoes[i-1] == 'R':
+                            if i-1 not in active:
+                                dominoes[i-1] = '.'
+                                nxt_active.remove(i-1)
+                else:
+                    if i < len(dominoes) - 1:
+                        if dominoes[i+1] == '.':
+                            dominoes[i+1] = 'R'
+                            nxt_active.add(i+1)
+                        elif dominoes[i+1] == 'L':
+                            if i+1 not in active:
+                                dominoes[i+1] = '.'
+                                nxt_active.remove(i+1)
+            active = nxt_active
+        return "".join(dominoes) 
