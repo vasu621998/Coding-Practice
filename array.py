97385461a6c7b9fdc4a80f1001d2818783136cfe
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-09-28 13:07:14 -0400

    85. Maximal Rectangle
    Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.

--- array.py
+++ array.py
@@ -1463,3 +1463,45 @@ class Solution:
 
 # Input: nums = [1,2,2]
 # Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        
+        '''
+        **For example 1 in que: it'll look like this after updating heights of building at each row**
+        Each row has heights of bars of histogram on that level of the row.
+        [1, 0, 1, 0, 0]
+        [2, 0, 2, 1, 1]
+        [3, 1, 3, 2, 2]
+        [4, 0, 0, 3, 0]  # Height of building at i=0, j= 0 is 4
+
+        '''
+        
+        '''This block updates the matrix. Each cell of every row stores height of the building(consecutive 1's from that cell to above cells).'''
+        
+        for i in range( rows):
+            for j in range(cols):
+                matrix[i][j] = int(matrix[i][j])
+                if i>0 and matrix[i][j] ==1 and matrix[i-1][j]!= 0:
+                    matrix[i][j] =  1 +  matrix[i-1][j] 
+            print(matrix[i])
+              
+        '''Here we perform the same operation as for Largest area of histogram, for every row, considering each row as array of heights of the histogram at that row level'''
+        maxArea = 0
+        for row in matrix:
+            row.append(0)
+            stack = [-1]
+            i = 0
+            for i, height in enumerate(row):
+                while(height<row[stack[-1]]):
+                    h = row[stack.pop()]
+                    width = i - 1 - stack[-1]
+                    maxArea = max(maxArea, h*width)
+                stack.append(i)
+        return maxArea
+
+# T : O(M*N)
+# S : O(1)
+
+# Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+# Output: 6
+# Explanation: The maximal rectangle is shown in the above picture.
