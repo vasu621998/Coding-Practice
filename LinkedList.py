1058acc3bd5c822d564d919937f82f312194a4a2
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-10-14 12:40:02 -0400

    2095. Delete the Middle Node of a Linked List
    You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
    
    The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
    
    For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.

--- LinkedList.py
+++ LinkedList.py
@@ -793,3 +793,35 @@ class DoublyLinkedList:
 
 # Input: head = [4,2,1,3]
 # Output: [1,2,3,4]
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next == None:
+            return None
+        elif head.next.next == None:
+            head.next = None
+            return head
+        else:
+        
+            slow = head
+            fast = head.next.next
+        
+            while(fast != None):
+            
+                fast = fast.next
+                if fast == None:
+                    slow.next = slow.next.next
+                    return head
+                fast = fast.next
+                slow = slow.next
+        
+            slow.next = slow.next.next
+            return head
+        
+# T : O(N)
+# S : O(1)
+
+#Input: head = [1,3,4,7,1,2,6]
+#Output: [1,3,4,1,2,6]
+#Explanation:
+#The above figure represents the given linked list. The indices of the nodes are #written below.
+#Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+#We return the new list after removing this node. 
