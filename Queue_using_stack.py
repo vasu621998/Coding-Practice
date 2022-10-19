e3b6fba6b5a35380f355293b46c828792958548a
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-10-18 21:14:41 -0400

    692. Top K Frequent Words
    Given an array of strings words and an integer k, return the k most frequent strings.
    
    Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.

--- Queue_using_stack.py
+++ Queue_using_stack.py
@@ -23,3 +23,26 @@ class MyQueue:
 
     def empty(self) -> bool:
         return not self.s1
+   
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        countMap = {}
+        for word in words:
+            countMap[word] = countMap.get(word, 0) + 1
+        
+		# Use negative count to make it a max heap
+        a = [(-1 * countMap[word], word) for word in countMap.keys()]
+        
+        heapq.heapify(a)
+        
+        res = [heapq.heappop(a)[1] for _ in range(k)]
+        
+		# Sort by count first, and lexicographically if count is same
+        return sorted(res, key=lambda x: (-countMap[x], x))        
+    
+# T : O(NlogK)
+# S : O(1)
+
+#Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+#Output: ["i","love"]
+#Explanation: "i" and "love" are the two most frequent words.
+#Note that "i" comes before "love" due to a lower alphabetical order.
