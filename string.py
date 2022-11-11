5598244c695c2840e211342059846e6ac962aee6
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-11-11 15:33:57 -0500

    316. Remove Duplicate Letters
    Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.

--- string.py
+++ string.py
@@ -644,3 +644,34 @@ class Solution:
 #Input: s = "leEeetcode"
 #Output: "leetcode"
 #Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+    def removeDuplicateLetters(self, s: str) -> str:
+        d=[0 for _ in range(26)]
+        for item in s:
+            j=ord(item)-97
+            d[j]+=1
+        stack=[]
+        d1=[False for _ in range(26)]
+        for item in s:
+            if(len(stack)==0 or stack[-1]<=item):
+                if(not d1[ord(item)-97]):
+                    stack.append(item)
+                d[ord(item)-97]-=1
+                d1[ord(item)-97]=True
+            else:
+                if(not d1[ord(item)-97]):     
+                    while(len(stack)!=0 and stack[-1]>item):
+                        j=ord(stack[-1])-97
+                        if(d[j]==0):
+                            break
+                        d1[ord(stack[-1])-97]=False
+                        stack.pop()
+                    stack.append(item)
+                d[ord(item)-97]-=1
+                d1[ord(item)-97]=True
+        return "".join(stack)
+    
+# T : O(N)
+# S : O(N)
+
+#Input: s = "bcabc"
+#Output: "abc"
