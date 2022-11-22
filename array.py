a414786b74a7f350ee355cba892cc7a759d9ffd1
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-11-22 11:01:04 -0500

    319. Bulb Switcher
    There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
    
    On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
    
    Return the number of bulbs that are on after n rounds.

--- array.py
+++ array.py
@@ -1964,3 +1964,16 @@ class Solution:
 # Input: words = ["abcw","baz","foo","bar","xtfn","abcdef"]
 # Output: 16
 # Explanation: The two words can be "abcw", "xtfn".
+    def bulbSwitch(self, n: int) -> int:
+        return int(math.sqrt(n))
+    
+# T : O(1)
+# S : O(1)
+
+#Input: n = 3
+#Output: 1
+#Explanation: At first, the three bulbs are [off, off, off].
+#After the first round, the three bulbs are [on, on, on].
+#After the second round, the three bulbs are [on, off, on].
+#After the third round, the three bulbs are [on, off, off]. 
+#So you should return 1 because there is only one bulb is on.
