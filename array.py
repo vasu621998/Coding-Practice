250cc292a5cacf97175619a161331ac2fd89dacb
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-11-28 13:28:41 -0500

    327. Count of Range Sum
    Given an integer array nums and two integers lower and upper, return the number of range sums that lie in [lower, upper] inclusive.
    
    Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j inclusive, where i <= j.

--- array.py
+++ array.py
@@ -2010,3 +2010,44 @@ class Solution:
 # Input: coins = [1,2,5], amount = 11
 # Output: 3
 # Explanation: 11 = 5 + 5 + 1
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        # First get the cumulative list
+        # takes O(n)
+        accumulated = nums.copy()
+        for i in range(0, len(nums)):
+            if i != 0:
+                accumulated[i] = nums[i] + accumulated[i-1]
+
+        # sort the cumulative list
+        # we do this because it's easy for later operations
+        # takes O(n logn)
+        new_acc = sorted(accumulated)
+
+        result = 0
+        num = 0
+
+        # takes O(n)        
+        for i in range(0, len(nums)):
+
+            # get how many subarrays are within the bound
+            # inside the loop, takes O(logn)
+            l = bisect_left(new_acc, lower)
+            r = bisect_right(new_acc, upper)
+            diff = r - l
+
+            result += diff
+            lower += nums[num]
+            upper += nums[num]
+            poped = bisect_left(new_acc, accumulated[num])
+            new_acc.pop(poped)
+            num += 1
+
+        # overall, takes O(n logn)
+        return result
+    
+# T : O(NlogN)
+# S : O(1)
+
+# Input: nums = [-2,5,-1], lower = -2, upper = 2
+# Output: 3
+# Explanation: The three ranges are: [0,0], [2,2], and [0,2] and their respective sums are: -2, -1, 2.
