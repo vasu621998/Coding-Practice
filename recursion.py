ebf09fd6d46e26bf4a6fae6027c8213c3e1d9b7d
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-12-10 22:53:17 -0500

    367. Valid Perfect Square
    Given a positive integer num, write a function which returns True if num is a perfect square else False.
    
    Follow up: Do not use any built-in library function such as sqrt.

--- recursion.py
+++ recursion.py
@@ -79,3 +79,7 @@ class Solution:
             return trees
         
         return build(1, n)   
+    def isPerfectSquare(self, num: int) -> bool:
+        if int((num**.5))**2 == num:
+            return True
+        return False
