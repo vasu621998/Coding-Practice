f2744d367bbe410bd3f9fc679e43d19a282b8421
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-12-13 18:35:01 -0500

    376. Wiggle Subsequence
    A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
    
    For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) alternate between positive and negative.
    In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences. The first is not because its first two differences are positive, and the second is not because its last difference is zero.
    A subsequence is obtained by deleting some elements (possibly zero) from the original sequence, leaving the remaining elements in their original order.
    
    Given an integer array nums, return the length of the longest wiggle subsequence of nums.

--- array.py
+++ array.py
@@ -2122,3 +2122,40 @@ class Solution:
 # Input: s = "book"
 # Output: true
 # Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+                ## O(n) solution
+        
+        if not nums:
+            return 0
+        if len(nums)<2:
+            return 1
+        
+        last_num = nums[0]
+        state=None
+        length=1
+        
+        for i in range(1,len(nums)):
+            if nums[i]!=last_num:
+                if last_num>num[i]:
+                    if state=="up" or state==None:
+                        state="down"
+                        length +=1
+                        last_num = nums[i]
+                    else: #state=="down"
+                        last_num = min(last_num, nums[i])
+                else:
+                    if state=="down" or state==None:
+                        state="up"
+                        length +=1
+                        last_num = nums[i]
+                    else: #state=="up"
+                        last_num = max(last_num, nums[i])
+        return length
+
+
+# T : O(N)
+# S : O(N)
+
+#Input: nums = [1,7,4,9,2,5]
+#Output: 6
+#Explanation: The entire sequence is a wiggle sequence with differences (6, -3, 5, #-7, 3).
