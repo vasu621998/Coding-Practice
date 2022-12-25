d5715f5722313ca5c254e232f4956edd36985d4f
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2022-12-25 00:27:19 -0500

    2389. Longest Subsequence With Limited Sum
    You are given an integer array nums of length n, and an integer array queries of length m.
    
    Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
    
    A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.

--- array.py
+++ array.py
@@ -2194,3 +2194,15 @@ class Solution:
 # Input: n = 3
 # Output: 5
 # Explanation: The five different ways are show above.
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        prefix = list(accumulate(sorted(nums)))
+        return [bisect_right(prefix, q) for q in queries]
+# T : O((M+N)log(M+N))
+# S : O(M+N)
+
+# Input: nums = [4,5,2,1], queries = [3,10,21]
+#Output: [2,3,4]
+#Explanation: We answer the queries as follows:
+#- The subsequence [2,1] has a sum less than or equal to 3. It can be proven that #2 is the maximum size of such a subsequence, so answer[0] = 2.
+#- The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven #that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+#- The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven #that 4 is the maximum size of such a subsequence, so answer[2] = 4.
