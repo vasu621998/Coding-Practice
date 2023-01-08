d3c372a89f1e10309dfee79ce563290ec99b75f8
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2023-01-08 17:09:06 -0500

    406. Queue Reconstruction by Height
    You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
    
    Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).

--- Queue_BFS.py
+++ Queue_BFS.py
@@ -190,3 +190,31 @@ class MyCircularQueue:
 # S : O(1)
 # Input: n = 10, pick = 6
 # Output: 6
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        output=[] 
+        
+        # sort the array in decreasing order of height 
+        # within the same height group, you would sort it in increasing order of k
+        # eg: Input : [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+        # after sorting: [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
+        people.sort(key=lambda x: (-x[0], x[1]))                
+        for a in people:
+            # Now let's start the greedy here
+            # We insert the entry in the output array based on the k value
+            # k will act as a position within the array
+            output.insert(a[1], a)
+        
+        return output  
+
+# Time - O(nlogn + n * n) - We sort the array in O(nlogn) and the greedy algorithm loop takes O(n * n) because of array insert operation.
+# Space - O(1) - If you exclude the output array.
+# Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+#Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+#Explanation:
+#Person 0 has height 5 with no other people taller or the same height in front.
+#Person 1 has height 7 with no other people taller or the same height in front.
+#Person 2 has height 5 with two persons taller or the same height in front, which #is person 0 and 1.
+#Person 3 has height 6 with one person taller or the same height in front, which #is person 1.
+#Person 4 has height 4 with four people taller or the same height in front, which #are people 0, 1, 2, and 3.
+#Person 5 has height 7 with one person taller or the same height in front, which #is person 1.
+#Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
