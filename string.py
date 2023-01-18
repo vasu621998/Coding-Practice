3c3ae8030a911ea88bd4282a9337839d23132694
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2023-01-18 10:04:54 -0500

    423. Reconstruct Original Digits from English
    Given a string s containing an out-of-order English representation of digits 0-9, return the digits in ascending order.

--- string.py
+++ string.py
@@ -737,3 +737,26 @@ class Solution:
 # Input: s = "00110"
 # Output: 1
 # Explanation: We flip the last digit to get 00111.
+    def originalDigits(self, s: str) -> str:
+        from collections import Counter
+        c = Counter(s)
+        nums = [0] * 10
+        nums[0] = c['z']
+        nums[2] = c['w']
+        nums[4] = c['u']
+        nums[6] = c['x']
+        nums[8] = c['g']
+        nums[1] = c['o'] - nums[0] - nums[2] - nums[4]
+        nums[3] = c['h'] - nums[8]
+        nums[5] = c['f'] - nums[4]
+        nums[7] = c['s'] - nums[6]
+        nums[9] = c['i'] - nums[5] - nums[6] - nums[8]
+        return ''.join(str(i) * n for i, n in enumerate(nums))
+
+# T : O(N)
+# S : O(1)
+
+# Given a string s containing an out-of-order English representation of digits 0-9, return the digits in ascending order.
+
+# Input: s = "owoztneoer"
+# Output: "012"
