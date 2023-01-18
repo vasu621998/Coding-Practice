83a1b8d1b5a5c9b7b006ea37db9e9b1a7bb0aedb
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2023-01-18 10:08:33 -0500

    424. Longest Repeating Character Replacement
    You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
    
    Return the length of the longest substring containing the same letter you can get after performing the above operations.

--- string.py
+++ string.py
@@ -760,3 +760,26 @@ class Solution:
 
 # Input: s = "owoztneoer"
 # Output: "012"
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        left = 0
+        count = {}
+        
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+
+            # Check this is a valid window
+			while (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+            
+            res = max(res, right - left + 1)
+        
+        return res
+
+# T : O(26N) = O(N)
+# S : O(N)
+
+# Input: s = "ABAB", k = 2
+# Output: 4
+# Explanation: Replace the two 'A's with two 'B's or vice versa.V
