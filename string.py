b17d864ab69dd6defdfff1382aeac9dfbe47f05a
Vasu Jayeshkumar Patel <98359519+vasu621998@users.noreply.github.com>
2023-01-23 17:11:59 -0500

    466. Count The Repetitions
    We define str = [s, n] as the string str which consists of the string s concatenated n times.
    
    For example, str == ["abc", 3] =="abcabcabc".
    We define that string s1 can be obtained from string s2 if we can remove some characters from s2 such that it becomes s1.
    
    For example, s1 = "abc" can be obtained from s2 = "abdbec" based on our definition by removing the bolded underlined characters.
    You are given two strings s1 and s2 and two integers n1 and n2. You have the two strings str1 = [s1, n1] and str2 = [s2, n2].
    
    Return the maximum integer m such that str = [str2, m] can be obtained from str1.

--- string.py
+++ string.py
@@ -802,3 +802,40 @@ class Solution:
 # Explanation:
 # The substring with start index = 0 is "cba", which is an anagram of "abc".
 # The substring with start index = 6 is "bac", which is an anagram of "abc".
+    def getMaxRepetitions(self, s1: str, n1: int, s2: str, n2: int) -> int:
+        rec, track = [0], defaultdict(int) 
+        ct = start = ptr1 = ptr2 = 0
+
+        if not set(s2).issubset(set(s1)): return 0
+
+        s1 = ''.join(char for char in s1 if char in set(s2))
+        
+        while True:
+            for char in s2:
+                ptr = s1.find(char, start)
+                if ptr == -1:
+                    ct += 1
+                    ptr = s1.find(char)
+                start = ptr+1
+            rec.append(ct + 1)
+
+            if rec[-1] > n1: return (len(rec)-2)//n2
+
+            if ptr not in track: track[ptr] = len(rec)-1
+            else: break
+        
+        cycleStart = rec[track[ptr]]
+        cycle1, cycle2 = ct+1 - cycleStart, len(rec)-1 - track[ptr]
+        rest = n1 - cycleStart
+        
+        rem = cycleStart + rest%cycle1
+
+        while rec[ptr2] <= rem: ptr2+= 1
+
+        return (cycle2 * (rest//cycle1) + ptr2-1)//n2
+
+# T : O(N)
+# S : O(N)
+
+# Input: s1 = "acb", n1 = 4, s2 = "ab", n2 = 2
+# Output: 2
